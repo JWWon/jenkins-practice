b5ad722fdb1330fa2e3767957bf7614514d28f9e
JWWon <wonjiwn@naver.com>
2018-05-23 21:10:13 +0900

    fix port number

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -9,4 +9,4 @@ docker stop jenkins-practice-2
 cd ${WORKSPACE}/jenkins/base
 
 docker build --build-arg CACHE_BUILD=${BUILD_NUMBER} -t jenkins-practice-2 .
-docker run -p 82:80 -d jenkins-practice-2
+docker run -p 80:82 -d jenkins-practice-2
