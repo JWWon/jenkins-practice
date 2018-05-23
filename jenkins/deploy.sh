5d26ee555b7121ff4000533f4f01a92903d3d792
JWWon <wonjiwn@naver.com>
2018-05-23 21:06:38 +0900

    add jenkins file

---
+++ jenkins/deploy.sh
@@ -0,0 +1,12 @@
+echo "WORKSPACE: "$1
+echo "BUILD_NUMBER: "$2
+
+WORKSPACE=$1
+BUILD_NUMBER=$2
+
+docker stop jenkins-practice-2
+
+cd ${WORKSPACE}/jenkins/base
+
+docker build --build-arg CACHE_BUILD=${BUILD_NUMBER} -t jenkins-practice-2 .
+docker run -p 82:80 -d jenkins-practice-2
